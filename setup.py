24348ec18e4bb551c2de3af92dea33ee188757e6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-02-03 17:11:56 -0700

    Cleanup and packaging

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering',
+]
+
+setup(
+    name='xtime',
+    long_description_content_type='text/markdown',
+    description='A Python package for managing time axis and different operations related to time with xarray',
+    long_description=long_description,
+    python_requires='>=3.6',
+    maintainer='Anderson Banihirwe',
+    maintainer_email='abanihi@ucar.edu',
+    classifiers=CLASSIFIERS,
+    url='https://github.com/NCAR/xtime',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    install_requires=install_requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    entry_points={},
+    keywords='xarray, cftime, time',
+    use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
+    setup_requires=['setuptools_scm', 'setuptools>=30.3.0'],
+)
