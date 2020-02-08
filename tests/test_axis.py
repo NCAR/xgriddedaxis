957ca519d3708e21d5e30250c95cffd360ad2dec
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-02-07 21:39:46 -0700

    Raise RuntimeError when time bounds are missing

--- tests/test_axis.py
+++ tests/test_axis.py
@@ -1,28 +1,32 @@
+import itertools
+
 import pytest
 import xarray as xr
 
 from xtimeutil import Axis
 from xtimeutil.testing import create_dataset
 
+# cftime config
+time_units = ('days since 1800-01-01', 'hours since 1800-01-01')
+calendars = ('standard', 'noleap', '360_day', 'all_leap', '365_day', 'proleptic_gregorian')
+decode_times = (False, True)
+inputs1 = [time_units, calendars, decode_times, (True,)]
+
+# pandas datetime config
+inputs2 = [('days since 1800-01-01',), ('standard',), (True, False), (False,)]
+
+combs1 = [element for element in itertools.product(*inputs1)]
+combs2 = [element for element in itertools.product(*inputs2)]
+
+parameters = combs1 + combs2
+
 
 @pytest.mark.parametrize(
-    'has_time_bounds, time_units, calendar, use_cftime, decode_times',
-    [
-        (True, 'days since 0001-01-01', 'noleap', True, True),
-        (True, 'days since 0001-01-01', 'noleap', True, False),
-        (False, 'days since 0001-01-01', 'noleap', True, True),
-        (True, 'hours since 1800-01-01', 'standard', False, True),
-        (False, 'hours since 1800-01-01', 'standard', False, True),
-        (True, 'hours since 1800-01-01', 'standard', False, False),
-    ],
+    'time_units, calendar, decode_times, use_cftime', parameters,
 )
-def test_init_axis(has_time_bounds, time_units, calendar, use_cftime, decode_times):
+def test_init_axis(time_units, calendar, decode_times, use_cftime):
     ds = create_dataset(
-        has_time_bounds=has_time_bounds,
-        time_units=time_units,
-        calendar=calendar,
-        use_cftime=use_cftime,
-        decode_times=decode_times,
+        time_units=time_units, calendar=calendar, use_cftime=use_cftime, decode_times=decode_times,
     )
     axis = Axis(ds)
     assert axis.metadata['is_time_decoded'] == decode_times
@@ -33,14 +37,19 @@ def test_init_axis(has_time_bounds, time_units, calendar, use_cftime, decode_tim
     'dataset, decode_times',
     [('rasm', False), ('rasm', True), ('air_temperature', False), ('air_temperature', True)],
 )
-def test_init_with_real_data(dataset, decode_times):
+def test_init_missing_bounds(dataset, decode_times):
     ds = xr.tutorial.open_dataset(dataset, decode_times=decode_times)
-    axis = Axis(ds)
-    assert axis.metadata['is_time_decoded'] == decode_times
-    assert axis.decoded_times.shape == ds.time.shape
+    with pytest.raises(RuntimeError):
+        _ = Axis(ds)
 
 
 def test_validate_time_coord():
     ds = create_dataset()
-    with pytest.raises(ValueError):
+    with pytest.raises(KeyError):
         _ = Axis(ds, 'times')
+
+
+def test_invalid_binding():
+    ds = create_dataset()
+    with pytest.raises(KeyError):
+        _ = Axis(ds, 'time', 'center')
--- xtimeutil/axis.py
+++ xtimeutil/axis.py
@@ -7,7 +7,9 @@ class Axis:
     An object that represents a time coordinate axis.
     """
 
-    def __init__(self, ds, time_coord_name='time'):
+    _bindings = {'left': np.min, 'right': np.max, 'middle': np.mean}
+
+    def __init__(self, ds, time_coord_name='time', binding='middle'):
         """
         Create a new Axis object from an input dataset
 
@@ -17,10 +19,27 @@ class Axis:
             Contains the relevant time coordinate information.
         time_coord_name : str, optional
             Name for time coordinate to use, by default 'time'
+        binding : {'left', 'right', 'middle'}, optional
+            Defines different ways a data tick could be bound to an interval.
+
+            - `left`: means that the data tick is bound to the left/beginning of
+              the interval or the lower time bound.
+
+            - `right`: means that the data tick is bound to the right/end of the
+               interval or the upper time bound.
+
+            - `middle`: means that the data tick is bound half-way through between
+               lower time bound and upper time bound.
         """
         _validate_time_coord(ds, time_coord_name)
         self._ds = ds
         self.metadata = {}
+        if binding in Axis._bindings:
+            self.metadata['binding'] = binding
+        else:
+            message = f'Could not find the Time Axis binding associated to `{binding}`. '
+            message += f'Possible options are: {list(Axis._bindings.keys())}'
+            raise KeyError(message)
         self.metadata['is_time_decoded'] = _is_time_decoded(ds[time_coord_name])
         self.metadata['time_coord_name'] = time_coord_name
         self.metadata.update(self._get_time_attrs())
@@ -35,25 +54,22 @@ class Axis:
             self.metadata['input_time_object_type'] = 'numeric'
             self._time = self._ds[self.metadata['time_coord_name']].copy()
 
-        if self.metadata['time_bounds_varname']:
-            if _is_time_decoded(self._ds[self.metadata['time_bounds_varname']]):
-                self._time_bounds = xr.coding.times.encode_cf_datetime(
-                    self._ds[self.metadata['time_bounds_varname']],
-                    units=self.metadata['units'],
-                    calendar=self.metadata['calendar'],
-                )[0]
-
-            else:
-                self._time_bounds = self._ds[self.metadata['time_bounds_varname']].copy()
+        if _is_time_decoded(self._ds[self.metadata['time_bounds_varname']]):
+            self._time_bounds = xr.coding.times.encode_cf_datetime(
+                self._ds[self.metadata['time_bounds_varname']],
+                units=self.metadata['units'],
+                calendar=self.metadata['calendar'],
+            )[0]
 
         else:
-            # Dummy time bounds
-            self._time_bounds = np.vstack((self._time, self._time)).T
+            self._time_bounds = self._ds[self.metadata['time_bounds_varname']].copy()
 
         self.decoded_times = xr.DataArray(
             dims=[self.metadata['time_coord_name']],
             data=xr.coding.times.decode_cf_datetime(
-                self._time_bounds.mean(axis=self.metadata['time_bounds_dim_axis_num']),
+                Axis._bindings[self.metadata['binding']](
+                    self._time_bounds, axis=self.metadata['time_bounds_dim_axis_num']
+                ),
                 self.metadata['units'],
                 self.metadata['calendar'],
             ),
@@ -93,18 +109,14 @@ class Axis:
             time_bounds_varname = encoding['bounds']
 
         else:
-            time_bounds_varname = None
+            message = f'Unable to infer the time coordinate boundary variable'
+            # TODO: Tell the user how to generate/provide time bounds.
+            raise RuntimeError(message)
 
-        if time_bounds_varname:
-            dims = set(self._ds[time_bounds_varname].dims)
-            time_coord_name = set([self.metadata['time_coord_name']])
-            time_bounds_dim = (dims - time_coord_name).pop()
-            time_bounds_dim_axis_num = self._ds[time_bounds_varname].get_axis_num(time_bounds_dim)
-
-        else:
-            time_bounds_dim = 'd2'  # Default/Dummy value
-            time_bounds_dim_axis_num = 1  # Default value
-
+        dims = set(self._ds[time_bounds_varname].dims)
+        time_coord_name = set([self.metadata['time_coord_name']])
+        time_bounds_dim = (dims - time_coord_name).pop()
+        time_bounds_dim_axis_num = self._ds[time_bounds_varname].get_axis_num(time_bounds_dim)
         return {
             'units': units,
             'calendar': calendar,
@@ -126,7 +138,7 @@ def _get_time_bounds_dims(metadata):
 
 def _validate_time_coord(ds, time_coord_name):
     if time_coord_name not in ds.variables:
-        raise ValueError(f'Could not find time coordinate name = {time_coord_name} in the dataset.')
+        raise KeyError(f'Could not find time coordinate name = {time_coord_name} in the dataset.')
 
 
 def _is_time_decoded(x):
@@ -134,11 +146,7 @@ def _is_time_decoded(x):
 
 
 def _datetime_object_type(x):
-    if _is_time_decoded(x):
-        if xr.core.common.contains_cftime_datetimes(x):
-            return 'cftime'
-        else:
-            return 'np_datetime'
-
+    if xr.core.common.contains_cftime_datetimes(x):
+        return 'cftime'
     else:
-        return 'numeric'
+        return 'np_datetime'
